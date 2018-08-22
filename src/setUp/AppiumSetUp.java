7e229e777b08baf87e53ee5bb0e62272eafc74e6
Praveen <deepak.praveen@ymail.com>
2018-08-22 12:52:52 +0530

    Launch code

---
+++ src/setUp/AppiumSetUp.java
@@ -0,0 +1,43 @@
+package setUp;
+import java.util.*;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class AppiumSetUp {
+	static AppiumDriver<MobileElement> driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("deviceName", "My Phone");
+		caps.setCapability("udid", "3058b45f"); //Give Device ID of your mobile phone
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("platformVersion", "6.0");
+		caps.setCapability("appPackage", "com.android.vending");
+		caps.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		caps.setCapability("noReset", "true");
+		
+		
+		
+		
+		try {
+			driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"),caps);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.quit();
+
+		
+	}
+
+}
